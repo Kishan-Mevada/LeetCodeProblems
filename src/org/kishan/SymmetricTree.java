9a9d4295910755fea01ec4dea309ba7052a080cb
Kishan Mevada <mevada.kishan@gmail.com>
2021-08-29 09:01:33 +0300

    Mirror tree iterative solution

--- src/org/kishan/SymmetricTree.java
+++ src/org/kishan/SymmetricTree.java
@@ -1,2 +1,50 @@
-package org.kishan;public class SymmetricTree {
+package org.kishan;
+
+import org.kishan.utils.TreeNode;
+
+/**
+ * Given a binary tree,check whether it is a mirror of itself
+ *
+ * Solution Algorithm
+ *
+ *  - if root is null return true
+ *  - variable p = root.left, q = root.right
+ *  - compare recursively the subtree
+ *  	 if both of node are null return true
+ *  	 if one of the node is null return false
+ *  	 if nodes value are not same return false
+ *  	 recursively compare the subtree
+ *  	 		p = p.left
+ *  	 	 	q = q.right
+ *  	 recursively compare the subtree
+ *  	 		p = p.right
+ *  	 		q = q.left
+ *
+ *
+ */
+public class SymmetricTree {
+
+	static boolean isSymmetric(TreeNode root){
+		if(root == null) return false;
+		return isSymmetric(root.getLeft(),root.getRight());
+	}
+	private static boolean isSymmetric(TreeNode left, TreeNode right) {
+		if (left == null && right == null) return true;
+		if(left == null || right == null) return false;
+		return left.getValue() == right.getValue()
+				&& isSymmetric(left.getLeft(),right.getRight())
+				&& isSymmetric(left.getRight(),right.getLeft());
+	}
+	public static void main(String[] args) {
+		TreeNode root = new TreeNode(1,
+				new TreeNode(2,new TreeNode(3,null,null),new TreeNode(4,null,null)),
+				new TreeNode(2,new TreeNode(4,null,null),new TreeNode(3,null,null)));
+		System.out.println("Is Symmetric Tree -> " + isSymmetric(root));
+
+		root = new TreeNode(1,
+				new TreeNode(2,new TreeNode(3,null,null),null),
+				new TreeNode(2,new TreeNode(4,null,null),null));
+
+		System.out.println("Is Symmetric Tree -> " + isSymmetric(root));
+	}
 }
--- src/org/kishan/utils/TreeNode.java
+++ src/org/kishan/utils/TreeNode.java
@@ -1,2 +1,30 @@
-package org.kishan.utils;public class TreeNode {
+package org.kishan.utils;
+
+public class TreeNode {
+	int value;
+	TreeNode left;
+	TreeNode right;
+	public TreeNode(int value, TreeNode left, TreeNode right) {
+		this.value = value;
+		this.left = left;
+		this.right = right;
+	}
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+	public TreeNode getLeft() {
+		return left;
+	}
+	public void setLeft(TreeNode left) {
+		this.left = left;
+	}
+	public TreeNode getRight() {
+		return right;
+	}
+	public void setRight(TreeNode right) {
+		this.right = right;
+	}
 }

ea3326198f0eecf530b68479bf7b875d65b50d8e
Kishan Mevada <mevada.kishan@gmail.com>
2021-09-04 16:47:16 +0300

    BST zigzag  level order

---
+++ src/org/kishan/year_2021/month_september/date_4/ZigZagBSTLevelOrder.java
@@ -0,0 +1,50 @@
+package org.kishan.year_2021.month_september.date_4;
+
+import org.kishan.utils.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 	Give the roots of binary tree, return the zigzag level order traversal of its nodes value
+ *
+ * 	-- Time : O(n) , where n is number of nodes
+ * 	-- Space : O(2 * n) = O(n), for list and queue
+ *
+ */
+public class ZigZagBSTLevelOrder {
+
+	static List<List<Integer>> levelOrder(TreeNode root){
+		if(root == null) return new ArrayList<>();
+		boolean isRight = true;
+		List<List<Integer>> result = new ArrayList<>();
+		Queue<TreeNode> queue = new LinkedList<>();
+		queue.add(root);
+
+		while(!queue.isEmpty()){
+			List<Integer> values = new ArrayList<>();
+			int size = queue.size();
+			for(int i = 0; i < size; i++) {
+				TreeNode node = queue.poll();
+				if(isRight){
+					values.add(node.getValue());
+				}
+				else{
+					values.add(0,node.getValue());
+				}
+				if(node.getLeft() != null) queue.add(node.getLeft());
+				if(node.getRight() != null) queue.add(node.getRight());
+
+			}
+			result.add(values);
+			isRight = !isRight;
+		}
+		return result;
+	}
+	public static void main(String[] args) {
+		TreeNode root = new TreeNode(3,new TreeNode(9,null,null),new TreeNode(20,new TreeNode(15,null,null),new TreeNode(7,null,null)));
+		System.out.println(levelOrder(root));
+	}
+}
